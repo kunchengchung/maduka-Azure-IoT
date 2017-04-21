4d98fde2a350607675e8cf75ded7b3d40ca51c2a
maduka <maduka@MADUKA-NB>
2017-04-21 23:06:18 +0800

    Add Notification Mobile Application

---
+++ Mobile/appNotification/MainActivity.cs
@@ -0,0 +1,58 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace appNotification
+{
+    using Gcm.Client;
+    using System;
+
+    [Activity(Label = "appNotification", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+
+
+        public static MainActivity instance = null;
+
+        private void CreateGCM()
+        {
+            try
+            {
+                // Check to ensure everything's setup right
+                GcmClient.CheckDevice(this);
+                GcmClient.CheckManifest(this);
+
+                // Register for push notifications
+                System.Diagnostics.Debug.WriteLine("Registering...");
+                GcmClient.Register(this, Constans.SENDER_IDS);
+            }
+            catch (Java.Net.MalformedURLException)
+            {
+                CreateAndShowDialog("There was an error creating the client. Verify the URL.", "Error");
+            }
+            catch (Exception e)
+            {
+                CreateAndShowDialog(e.Message, "Error");
+            }
+        }
+
+        private void CreateAndShowDialog(String message, String title)
+        {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+            builder.SetMessage(message);
+            builder.SetTitle(title);
+            builder.Create().Show();
+        }
+    }
+}
+
